730f941a64c983fe9efeffdf340c1f78e0a471c1
saimpot <saimpot@gmail.com>
2023-08-12 02:07:22 +0300

    [SPD-6] Update use model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,46 +2,87 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Barryvdh\LaravelIdeHelper\Eloquent;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\PersonalAccessToken;
 
+/**
+ * App\Models\User.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property Carbon|null $email_verified_at
+ * @property mixed $password
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read DatabaseNotificationCollection<int, DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read Collection<int, PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ *
+ * @method static UserFactory factory($count = null, $state = [])
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereEmail($value)
+ * @method static Builder|User whereEmailVerifiedAt($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereName($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereUpdatedAt($value)
+ *
+ * @mixin Eloquent
+ */
 class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    public const COLUMN_ID = 'id';
+    public const COLUMN_NAME = 'name';
+    public const COLUMN_EMAIL = 'email';
+    public const COLUMN_EMAIL_VERIFIED_AT = 'email_verified_at';
+    public const COLUMN_REMEMBER_TOKEN = 'remember_token';
+    public const COLUMN_PASSWORD = 'password';
+    public const COLUMN_CREATED_AT = 'created_at';
+    public const COLUMN_UPDATED_AT = 'updated_at';
+
+    protected $table = 'users';
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        self::COLUMN_NAME,
+        self::COLUMN_EMAIL,
+        self::COLUMN_PASSWORD,
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    protected $guarded = [
+        self::COLUMN_ID,
+        self::COLUMN_REMEMBER_TOKEN,
+        self::COLUMN_CREATED_AT,
+        self::COLUMN_UPDATED_AT,
+    ];
+
     protected $hidden = [
-        'password',
-        'remember_token',
+        self::COLUMN_PASSWORD,
+        self::COLUMN_REMEMBER_TOKEN,
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password'          => 'hashed',
+        self::COLUMN_EMAIL_VERIFIED_AT => 'datetime',
+        self::COLUMN_PASSWORD          => 'hashed',
     ];
 }
