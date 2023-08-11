5be913c038c950b127b3b302be68f3f1f4d3a7ba
saimpot <saimpot@gmail.com>
2023-08-12 02:12:28 +0300

    [SPD-6] Added some default routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', static fn () => 'This application does not have a frontend. Use the API.')->name('landing');
+Route::get('/login', static fn () => 'You have been redirected here, because you are not logged in. Please use the artisan command "api:create-user" to create a user and use the token provided to authenticate yourself with the API.')
+    ->name('login');
