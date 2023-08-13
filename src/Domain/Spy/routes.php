8ef457c1e8d445d0599fe133a02d5eb67c374902
saimpot <saimpot@gmail.com>
2023-08-14 01:58:42 +0300

    [SPD-6] Add separate routes file in Spy entity for better SoC

---
+++ src/Domain/Spy/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+use App\Http\Controllers\Api\Spy\SpyCollectionController;
+use App\Http\Controllers\Api\Spy\SpyCreateController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::group(['prefix' => 'spy'], static function () {
+        Route::post('/create', SpyCreateController::class)->name('spy.create');
+
+        Route::middleware(['throttle:10,1'])
+            ->get('/random', [SpyCollectionController::class, 'random'])
+            ->name('spy.find');
+
+        Route::get('/all', [SpyCollectionController::class, 'all'])->name('spy.all');
+    });
+});
